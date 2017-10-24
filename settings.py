eb75e6c7be65d722a8489f680b6a92b5ff44b566
Volodymyr M <vmiroshnik301290@gmail.com>
2017-10-24 16:42:46 +0300

    remove proxy from settings

--- settings.py
+++ settings.py
@@ -3,4 +3,5 @@
 
 token = r'453680726:AAFUo4SC4zqN2GSthS6r-aS5iTNJ_jkU2So'
 URL = r'https://api.telegram.org/bot'
-
+proxy = {'https': 'https://vmiroshnik:13lyuba@10.0.0.254:3128',
+         'http': 'http://vmiroshnik:13lyuba@10.0.0.254:3128'}
