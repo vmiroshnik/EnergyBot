0e23cb7d44742996d4864e237a093cff2b8e6431
Volodymyr Miroshnyk <vmiroshnik301290@gmail.com>
2017-10-26 08:18:33 +0300

    Delete settings.py

--- settings.py
+++
@@ -1,7 +0,0 @@
-# bot url t.me/UAEnergyBot
-
-
-token = r'453680726:AAFUo4SC4zqN2GSthS6r-aS5iTNJ_jkU2So'
-URL = r'https://api.telegram.org/bot'
-proxy = {'https': 'https://vmiroshnik:13lyuba@10.0.0.254:3128',
-         'http': 'http://vmiroshnik:13lyuba@10.0.0.254:3128'}
